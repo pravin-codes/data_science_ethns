558faac18e34e97cb27606c6eb8ca550c72e64d8
Pravin Gaikwad <pravingaikwadwork08@gmail.com>
2024-07-13 01:24:18 +0530

    Create string_multiply.py

---
+++ string_multiply.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        res = int(num1) * int(num2)
+        str_res = str(res)
+        return str_res
+        result = multiply("2", "3")
+        print(result) 
